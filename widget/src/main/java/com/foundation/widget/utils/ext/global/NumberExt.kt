0b69533cff65e240194badf393e290c8bf599f8c
WangNeng <873456775@qq.com>
2022-11-10 15:06:38 +0800

    增加抹去多余小数0的方法

--- widget/src/main/java/com/foundation/widget/utils/ext/global/NumberExt.kt
+++ widget/src/main/java/com/foundation/widget/utils/ext/global/NumberExt.kt
@@ -4,14 +4,25 @@ import java.math.BigDecimal
 import java.text.DecimalFormat
 
 /**
- * 精确到2位小数
+ * 精确到2位小数，并保留.00
  */
 fun <T : Number?> T?.toStringTo2Decimal(): String {
-    if (this == null) {
+    if (this == null || this == 0) {
         return "0.00"
     }
     //使用0.00不足位补0，#.##仅保留有效位
     return DecimalFormat("0.00").format(this.toDouble())
+}
+
+/**
+ * 精确到2位小数，并抹去多余的小数0
+ */
+fun <T : Number?> T?.toStringEraseZero(): String {
+    if (this == null || this == 0) {
+        return "0"
+    }
+    //使用0.00不足位补0，#.##仅保留有效位
+    return DecimalFormat("#.##").format(this.toDouble())
 }
 
 fun <T : Number> T.toBigDecimal(): BigDecimal {
