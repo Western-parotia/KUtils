7f1b37d26335aa6fa8022c5acb551ef329ebe7bd
WangNeng <873456775@qq.com>
2022-11-12 11:34:50 +0800

    对显示2位小数toStringTo2Decimal计算逻辑调整

--- widget/src/main/java/com/foundation/widget/utils/ext/global/NumberExt.kt
+++ widget/src/main/java/com/foundation/widget/utils/ext/global/NumberExt.kt
@@ -1,28 +1,36 @@
 package com.foundation.widget.utils.ext.global
 
 import java.math.BigDecimal
+import java.math.RoundingMode
 import java.text.DecimalFormat
 
 /**
  * 精确到2位小数，并保留.00
+ * @param mode 舍入模式（如果超出2位），默认向下取值
  */
-fun <T : Number?> T?.toStringTo2Decimal(): String {
+@JvmOverloads
+fun <T : Number?> T?.toStringTo2Decimal(mode: RoundingMode = RoundingMode.DOWN): String {
     if (this == null || this == 0) {
         return "0.00"
     }
     //使用0.00不足位补0，#.##仅保留有效位
-    return DecimalFormat("0.00").format(this.toDouble())
+    val df = DecimalFormat("0.00")
+    df.roundingMode = mode
+    return df.format(this.toDouble())
 }
 
 /**
  * 精确到2位小数，并抹去多余的小数0
+ * @param mode 舍入模式（如果超出2位），默认向下取值
  */
-fun <T : Number?> T?.toStringEraseZero(): String {
+fun <T : Number?> T?.toStringEraseZero(mode: RoundingMode = RoundingMode.DOWN): String {
     if (this == null || this == 0) {
         return "0"
     }
     //使用0.00不足位补0，#.##仅保留有效位
-    return DecimalFormat("#.##").format(this.toDouble())
+    val df = DecimalFormat("#.##")
+    df.roundingMode = mode
+    return df.format(this.toDouble())
 }
 
 fun <T : Number> T.toBigDecimal(): BigDecimal {
